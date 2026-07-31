425ef4f063ff75b2722c973e34c33670dda265d6
kc2024sr-crypto <kc2024sr@gmail.com>
2026-07-31 14:23:26 -0500

    eliminar node_modulos del repositorio

--- app.js
+++ app.js
@@ -1,14 +1,13 @@
 import express from 'express';
-import {configDotenv} from "dotenv"
-configDotenv()
+import "dotenv/config"
 const app = express();  
 const port = process.env.PUERTO || 3000; 
 
 app.get("/", (_, res) => { 
-    res.send("Aprendicez ficha 3407186"); 
+    res.send("Aprendicez ficha 3407186 SENA"); 
 });
 
 app.listen(port, () => { 
-    console.log( `SERVIDOR: https://localhost: ${port}`); 
+    console.log( `SERVIDOR: https://localhost:${port}`); 
 }); 
 
